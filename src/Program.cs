58a9f28e815801102736a7cd6fefb582f6dbd97b
Sourav Kayal <sourav.kayal@yahoo.com>
2023-06-01 19:03:18 -0700

    comment added
    Added comment in each function and modified some logic of hashing

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,43 @@
 ﻿
 using System.Collections;
 using System.Security.Cryptography;
+using System.Text;
 
+/// <summary>
+/// Implementation of bloom filter 
+/// </summary>
 public class BloomFilter
 {
     private BitArray bitArray;
     private int size;
     private int numHashFunctions;
 
-    public BloomFilter(int size, int expectedElements)
+    /// <summary>
+    /// Default constructor with static value
+    /// </summary>
+    public BloomFilter()
+    {
+        this.size = 1000;
+        this.bitArray = new BitArray(size);
+        this.numHashFunctions = 5;
+    }
+
+    /// <summary>
+    /// Constructor to take size of filter and number of hashing to be applied
+    /// </summary>
+    /// <param name="size">Size of the bloom filter</param>
+    /// <param name="hashFunctionNumber">Number of hashing times</param>
+    public BloomFilter(int size, int hashFunctionNumber)
     {
         this.size = size;
         this.bitArray = new BitArray(size);
-        this.numHashFunctions = OptimalNumHashFunctions(size, expectedElements);
+        this.numHashFunctions = hashFunctionNumber;
     }
 
+    /// <summary>
+    /// Add items to bloom filter
+    /// </summary>
+    /// <param name="item">Item to be pushed in bloom filter</param>
     public void Add(string item)
     {
         byte[] itemBytes = System.Text.Encoding.UTF8.GetBytes(item);
@@ -25,9 +48,14 @@ public class BloomFilter
         }
     }
 
+    /// <summary>
+    /// Check whether the item exist in filter
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
     public bool Contains(string item)
     {
-        byte[] itemBytes = System.Text.Encoding.UTF8.GetBytes(item);
+        byte[] itemBytes = Encoding.UTF8.GetBytes(item);
         for (int i = 0; i < numHashFunctions; i++)
         {
             int hash = ComputeHash(itemBytes, i);
@@ -37,22 +65,22 @@ public class BloomFilter
         return true;
     }
 
+    /// <summary>
+    /// Compute hash of the item which need to check against bloom filter
+    /// </summary>
+    /// <param name="itemBytes"></param>
+    /// <param name="hashIndex"></param>
+    /// <returns></returns>
     private int ComputeHash(byte[] itemBytes, int hashIndex)
     {
-        using (var md5 = MD5.Create())
+        using (var sha1 = SHA1.Create())
         {
-            byte[] hashBytes = md5.ComputeHash(itemBytes);
+            byte[] hashBytes = sha1.ComputeHash(itemBytes);
             int hash = BitConverter.ToInt32(hashBytes, 0);
             return Math.Abs(hash + hashIndex) % size;
         }
     }
 
-    private int OptimalNumHashFunctions(int size, int expectedElements)
-    {
-        // Formula: k = (m / n) * ln(2)
-        double numHashes = (size / (double)expectedElements) * Math.Log(2);
-        return (int)Math.Ceiling(numHashes);
-    }
 }
 
 
@@ -62,9 +90,9 @@ public class Program
     {
 
         int filterSize = 1000;
-        int expectedElements = 100;
+        int hashTines = 5;
 
-        BloomFilter filter = new BloomFilter(filterSize, expectedElements);
+        BloomFilter filter = new BloomFilter(filterSize, hashTines);
 
         // Adding items to the filter
         filter.Add("apple");
